ea18c2f059e099f00408f2f87b839fdaf83218b6
vahuja8 <vahuja8@mycentennial.ca>
2024-11-28 10:59:32 -0500

    working build for api for assignemnt 4

--- Wk12_Assign4Working/src/main/java/com/va/week12/Wk12Assign4WorkingApplication.java
+++ Wk12_Assign4Working/src/main/java/com/va/week12/Wk12Assign4WorkingApplication.java
@@ -2,19 +2,10 @@ package com.va.week12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(
-		scanBasePackages={"com.va.week12", 
-		"com.va.week12.controller", 
-		"com.va.week12.model",
-		"com.va.week12.service"}
-)
-@ComponentScan("com.va.week12")
-@EntityScan(basePackages = {"com.va.week12.service"})
 
+@SpringBootApplication
 public class Wk12Assign4WorkingApplication {
 
 	public static void main(String[] args) {
